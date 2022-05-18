2d98ae2c0a61ae1e0811d6d695730d803b9fc50c
echo <1342525123@qq.com>
2022-05-18 23:12:52 +0800

    dubbo 项目中base目录的提交

--- dubbo-provider/src/main/java/com/echo/provider/ProviderMain.java
+++ dubbo-provider/src/main/java/com/echo/provider/ProviderMain.java
@@ -3,8 +3,10 @@ package com.echo.provider;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableDubbo
+@EnableHystrix
 @SpringBootApplication
 public class ProviderMain {
     public static void main(String[] args) {
--- dubbo-provider/src/main/java/com/echo/provider/service/ProviderServiceImpl.java
+++ dubbo-provider/src/main/java/com/echo/provider/service/ProviderServiceImpl.java
@@ -2,15 +2,18 @@ package com.echo.provider.service;
 
 import com.echo.dao.ProviderTestDTO;
 import com.echo.service.IProviderService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.apache.dubbo.config.annotation.Service;
-
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-@Service        //  这里的service 是dubbo包下的 注解
+@Service        // 暴露服务的接口  这里的service 是dubbo包下的 注解
 public class ProviderServiceImpl implements IProviderService {
+    @HystrixCommand
     public List<ProviderTestDTO> queryList() {
+        System.out.println("我是provider service");
         List<ProviderTestDTO> list = new ArrayList<ProviderTestDTO>();
 
         ProviderTestDTO testDTO = new ProviderTestDTO();
@@ -25,6 +28,16 @@ public class ProviderServiceImpl implements IProviderService {
 
         list.add(testDTO);
         list.add(testDTO2);
+        // 这里模仿 超时 时间  接口默认调用的时间是 1000 ms  也可以直接在配置文件 中 定义 超时时间
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        // 模拟服务降级的处理
+        if (Math.random() > 0.1) {
+            throw new RuntimeException();
+        }
         return list;
     }
 }
